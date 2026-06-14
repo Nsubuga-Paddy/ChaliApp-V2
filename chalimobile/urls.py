e54abfe48aab26981a7f2fd644a0a5199ca59599
Nsubuga Paddy <paddynsubuga48@gmail.com>
2026-06-14 13:29:52 +0300

    improved url commands for images

--- chalimobile/urls.py
+++ chalimobile/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +11,13 @@ urlpatterns = [
     path('api/', include('operations.urls')),
 ]
 
-if settings.DEBUG or getattr(settings, 'SERVE_MEDIA', False):
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+elif getattr(settings, 'SERVE_MEDIA', False):
+    urlpatterns += [
+        re_path(
+            r'^media/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.MEDIA_ROOT},
+        ),
+    ]
